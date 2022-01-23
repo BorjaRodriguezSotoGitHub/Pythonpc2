15bb5da776d984cb3f308e89e9b6fa348c780f21
BorjaRodriguezSotoGitHub <78815253+BorjaRodriguezSotoGitHub@users.noreply.github.com>
2022-01-23 20:03:53 +0100

    Update P2MaquinaPesada.py

--- P2MaquinaPesada.py
+++ P2MaquinaPesada.py
@@ -4,7 +4,8 @@ import os
 os.system('git clone https://github.com/CDPS-ETSIT/practica_creativa2.git')
 
 
-os.system('pip3 install -r practica_creativa2/bookinfo/src/productpage/requirements.txt')
+os.system('pip install -r practica_creativa2/bookinfo/src/productpage/requirements.txt')
+os.system('pip install --upgrade requests')
 
 os.environ['GROUP_NAME'] = 'Grupo 35'
 titlename = os.environ['GROUP_NAME']
